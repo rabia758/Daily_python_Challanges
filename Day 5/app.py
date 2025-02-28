c7597f0d7dfa5f15642f8c273d98e6d76831585a
Rabia_Rizwan <165579059+rabia758@users.noreply.github.com>
2025-02-28 16:50:01 +0500

    Day 5

---
+++ Day 5/app.py
@@ -0,0 +1,12 @@
+#user input
+num = int(input("Please Enter Your Number🔢:  "))
+
+# ctreate variable for sum
+sum = 0
+
+# use for loop for sum
+for i in range(1,num +1):
+    sum += i
+
+# for result
+print(f"This is number you Enter:  {num} , \nThis is the result: 👉 {sum}")    
